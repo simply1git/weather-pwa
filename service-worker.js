02cf716e93ba1f6bc6a3191aab501eb7fc60c75e
simply1 <157337766+simply1git@users.noreply.github.com>
2025-06-27 20:47:31 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,18 @@
+const CACHE_NAME = "weather-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/forecast.js",
+  "/manifest.json",
+  "/icon.png"
+];
+
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("weather-app").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json",
-        "./icon.png"
-      ]);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
